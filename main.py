3518b510e1de6cb87bcac3ad24874d8fcda23ddd
Mauro M <hello@maurom.dev>
2022-07-08 18:09:52 +0100

    style(main): PEP 8 E501 compliance

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ print(f"\n--- SAVE FOLDER ---\n{folder_name}")
 
 print("\n--- START UNPACK ---")
 try:
-    # separated into two because PowerShell is fucking stupid and doesnt understand basic fucking sense (i spend a little time on this :D)
+    # separated into two because PowerShell is fucking stupid and doesnt understand basic fucking sense (i spend a
+    # little time on this :D)
     call = "cd 'C:/Program Files/gtautil-2.2.7';"
     call2 = f".\gtautil extractarchive --input '{rpf_path}' --output '{output_path}'"
     subprocess.run(['powershell', call, call2])
